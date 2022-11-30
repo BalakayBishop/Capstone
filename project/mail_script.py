b29fafac1a8348efaaaf0d92fdf675b0b31611b8
Blake Bishop <balakaybishop@gmail.com>
2022-11-29 18:28:11 -0800

    Local: made changes to the requirements, things to do, and the mail script for upgrades later.

--- project/mail_script.py
+++ project/mail_script.py
@@ -8,12 +8,11 @@ email_add = os.getenv('EMAIL_USER')
 email_pass = os.getenv('GMAIL_APP_PASSWORD')
 rcvr_email = os.getenv('RCVR_EMAIL')
 
-subject = 'Testing new automated email'
-body = "It's Blake! I am testing an automated email system that I am going to incorporate for the website!"
-message = "Subject: {0}\n\n{1}".format(subject, body)
+subject = ""
+body = ""
+message = f"Subject: {subject}\n\n{body}"
 
-server = smtplib.SMTP('smtp.gmail.com',587)
+server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(email_add, email_pass)
 server.sendmail(email_add, rcvr_email, message)
-print('mail sent')
