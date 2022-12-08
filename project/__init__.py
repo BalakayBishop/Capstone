173470c1893f9df3cddd8ba17063cec701280a32
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-12-08 12:34:16 -0800

    Local: added the HTML templates for all the webpages, added some CSS for navbar

--- project/__init__.py
+++ project/__init__.py
@@ -5,10 +5,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from dotenv import load_dotenv, find_dotenv
 
+# ------------------------------------------------------------------------------------------------
+
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 load_dotenv(find_dotenv())
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
@@ -16,3 +17,12 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 Migrate(app, db)
+
+# ------------------------------------------------------------------------------------------------
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+# ------------------------------------------------------------------------------------------------
+
