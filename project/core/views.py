9def756219bd97254edcbfb410bc37d2babe9778
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-12-09 09:57:34 -0800

    Local: added view for about page, fixed/refactored the more about me button on home page

--- project/core/views.py
+++ project/core/views.py
@@ -5,3 +5,8 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     return render_template('index.html')
+
+
+@core.route('/about')
+def about():
+    return render_template('about.html')
