75d975fce0724c911ba33180385515e8235c0011
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2023-01-10 23:12:42 -0800

    Local: completely reworking all templates and styles for those templates, deleted unnecessary files, reworked the navbar on all pages to be a Bootstrap navbar

--- project/core/views.py
+++ project/core/views.py
@@ -10,3 +10,23 @@ def index():
 @core.route('/about')
 def about():
     return render_template('about.html')
+
+
+@core.route('/contact_me')
+def contact():
+    return render_template('contact.html')\
+    
+
+@core.route('/appointment')
+def appointment():
+    return render_template('appointment.html')
+
+
+@core.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+
+@core.route('/account')
+def account():
+    return render_template('account.html')
