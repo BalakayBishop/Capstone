37125697c7b5f617fd62493605edac3b214976f7
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2023-01-10 17:41:53 -0800

    Local: reconfigured db to connect to MySQL instead of SQLite

--- project/config.py
+++ project/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(find_dotenv())
-SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+SQLALCHEMY_DATABASE_URI = f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 SQLALCHEMY_TRACK_MODIFICATION = False
 SECRET_KEY = os.getenv('SECRET_KEY')
 FLASK_ENV = 'development'
