3bc93d4bf56902a7f6466a742702210a9e3a65a9
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2023-01-16 12:59:33 -0800

    Local: added some jQuery for form validation

--- project/static/main.js
+++ project/static/main.js
@@ -9,28 +9,96 @@ $(document).ready(function(){
 		window.location.href = '/appointment'
 	})
 	
+	$('#contact-name').on('input', function() {
+		if ($('#contact-name').val() === '') {
+			$('#contact-name').removeClass('success fail')
+		}
+		else {
+			$('#contact-name').removeClass('fail')
+			$('#contact-name').addClass('success')
+		}
+	});
+	
+	$('#contact-email').on('input', function() {
+		if ($('#contact-email').val() === '') {
+			$('#contact-email').removeClass('success fail')
+		}
+		else {
+			$('#contact-email').removeClass('fail')
+			$('#contact-email').addClass('success')
+		}
+	});
+	
+	$('#message').on('input', function() {
+		if ($('#message').val() === '') {
+			$('#message').removeClass('success-ta fail')
+		}
+		else {
+			$('#message').removeClass('fail')
+			$('#message').addClass('success-ta')
+		}
+	});
+	
 	$('#form-button').on('click', function(e1) {
 		e1.preventDefault()
 		let name = $('#contact-name').val()
 		let email = $('#contact-email').val()
 		let message = $('#message').val()
 		
-		console.log(name, email, message)
-		// $.ajax({
-		// 	url: '',
-		// 	type: '',
-		// 	contentType: '',
-		// 	data: JSON.stringify({
-		// 		name: name,
-		// 		email: email,
-		// 		message: message
-		// 	}),
-		// 	success: function(response) {
-		//
-		// 	},
-		// 	fail: function(response) {
-		//
-		// 	}
-		// })
-	})
+		if (name === '' || email === '' || message === '') {
+			if (name === '') {
+				$('#contact-name').addClass('fail')
+			}
+			
+			if (email === '') {
+				$('#contact-email').addClass('fail')
+			}
+			
+			if (message === '') {
+				$('#message').addClass('fail')
+			}
+		}
+		else {
+			$('#contact-form')[0].reset()
+			$('#contact-name, #contact-email').removeClass('success fail')
+			$('#message').removeClass('success-ta fail')
+			
+			// $.ajax({
+			// 	url: '',
+			// 	type: '',
+			// 	contentType: '',
+			// 	data: JSON.stringify({
+			// 		name: name,
+			// 		email: email,
+			// 		message: message
+			// 	}),
+			// 	success: function(response) {
+			//
+			// 	},
+			// 	fail: function(response) {
+			//
+			// 	}
+			// })
+		}
+		
+		$('#contact-name').on('input', function() {
+			if ($('#contact-name').val() === '') {
+				$('#contact-name').removeClass('success fail')
+			}
+			else {
+				$('#contact-name').removeClass('fail')
+				$('#contact-name').addClass('success')
+			}
+		});
+		
+		$('#contact-email').on('input', function() {
+			$('#contact-email').removeClass('fail')
+			$('#contact-email').addClass('success')
+		});
+		
+		$('#message').on('input', function() {
+			$('#message').removeClass('fail')
+			$('#message').addClass('success-ta')
+		});
+	});
 });
