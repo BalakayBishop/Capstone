d4d3c92598f70e304e707b98540712b54f928612
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2023-02-09 15:16:21 -0800

    Local: completed my data massaging for turning the tuple of objects into list of dictionaries

--- project/core/methods.py
+++ project/core/methods.py
@@ -1,7 +1,19 @@
 def convert(query):
-	print(query)
-	lst = ()
+	# print(query)
+	lst = list()
 	i = 0
-	if len(query) is not 0:
-		pass
+	if len(query) != 0:
+		for obj in query:
+			lst.append({"post_id": obj.post_id, "post_title": obj.post_title, "post_body": obj.post_body,
+						"post_date": obj.post_date, "comments": []})
+			for comment in obj.comments:
+				lst[i]['comments'].append({
+					"comment_id": comment.comment_id,
+					"comment_body": comment.comment_body,
+					"comment_date": comment.comment_date
+				})
+			i += 1
+		for i in lst:
+			print(i)
+		return lst
 	return None
